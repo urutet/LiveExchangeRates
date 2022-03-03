75254c933c3710e4b234aba616add704ec41de64
Ilya Yushkevich <ilyshka88@gmail.com>
2022-03-03 09:15:39 +0300

    Project added

---
+++ LiveExchangeRates/CustomUIElements/ExchangeRateTableViewCell.swift
@@ -0,0 +1,76 @@
+//
+//  ExchangeRateTableViewCell.swift
+//  LiveExchangeRates
+//
+//  Created by Yushkevich Ilya on 20.02.22.
+//
+
+import UIKit
+import SnapKit
+
+final class ExchangeRateTableViewCell: UITableViewCell {
+
+    // MARK: - Properties
+    // MARK: Public
+    // MARK: Private
+    private let mainView = UIView()
+    private let stackView = UIStackView()
+    private let nameLabel = UILabel()
+    private let rateLabel = UILabel()
+    
+    // MARK: - Lifecycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+        addSubviews()
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        mainView.layer.cornerRadius = 5
+    }
+    // MARK: - API
+    func setProps(item: ExchangeRate) {
+        nameLabel.text = item.name
+        rateLabel.text = "$\(item.price_usd ?? 0.0)"
+    }
+    
+    // MARK: - Setups
+    private func setupUI() {
+        contentView.backgroundColor = .none
+        mainView.backgroundColor = UIColor(red: 37/255, green: 35/255, blue: 51/255, alpha: 1.0)
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        nameLabel.textColor = .white
+        rateLabel.textColor = .white
+    }
+    
+    private func addSubviews() {
+        contentView.addSubview(mainView)
+        mainView.addSubview(stackView)
+        stackView.addArrangedSubview(nameLabel)
+        stackView.addArrangedSubview(rateLabel)
+    }
+    
+    private func addConstraints() {
+        mainView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(10)
+            make.top.equalTo(contentView).inset(5)
+            make.height.equalTo(50)
+        }
+        
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Helpers
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        //super.setSelected(selected, animated: animated)
+    }
+
+}
